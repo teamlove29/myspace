4a266c88e38280d929c3bafb726ac909e3ce42e0
TeamMarutthep <TeamMarutthep@gmail.com>
2020-06-03 11:56:16 +0700

    befor manager folder

--- backoffice/src/components/pages/TestPage.js
+++ backoffice/src/components/pages/TestPage.js
@@ -1,48 +1,42 @@
-import React, { useEffect, useState, useMemo } from 'react'
+import React, { useEffect, useState, useMemo } from "react";
 import { NavLink } from "react-router-dom";
-import BootstrapTable from 'react-bootstrap-table-next';
-import paginationFactory, { PaginationProvider } from 'react-bootstrap-table2-paginator';
+import BootstrapTable from "react-bootstrap-table-next";
+import paginationFactory, {
+  PaginationProvider,
+} from "react-bootstrap-table2-paginator";
 import { useHistory } from "react-router-dom";
-import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
-import filterFactory, { selectFilter  } from 'react-bootstrap-table2-filter';
-import overlayFactory from 'react-bootstrap-table2-overlay';
-import axios from 'axios';
-import {
-  Table,
-  Card,
-
-} from 'react-bootstrap';
+import ToolkitProvider, { Search } from "react-bootstrap-table2-toolkit";
+import filterFactory, { selectFilter } from "react-bootstrap-table2-filter";
+import overlayFactory from "react-bootstrap-table2-overlay";
+import axios from "axios";
+import { Table, Card } from "react-bootstrap";
 import { Pagination } from "../_metronic/_partials/controls";
 
 export default function TestPage() {
-
   const [member, setMember] = useState([]);
   const [err, setErr] = useState();
   const history = useHistory();
   const { SearchBar } = Search;
 
-
   useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/users")
-      .then(response => {
-        setMember(response.data)
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        setMember(response.data);
       })
-      .catch(err => {
-        setErr({ err: err })
-      })
-  }, [])
-
-
+      .catch((err) => {
+        setErr({ err: err });
+      });
+  }, []);
 
   const Avater = (row) => {
-    const TextAvatar = row.substring(0,1)
+    const TextAvatar = row.substring(0, 1);
     return (
       <div class="symbol ">
         <span class="symbol-label font-size-h6">{TextAvatar}</span>
       </div>
-
-    )
-  }
+    );
+  };
 
   const actionButton = () => {
     return (
@@ -50,7 +44,7 @@ export default function TestPage() {
         <i class="flaticon2-gear"></i>
       </button>
     );
-  }
+  };
 
   const iconOnline = () => {
     return (
@@ -58,78 +52,113 @@ export default function TestPage() {
         <span class="label label-dot label-success"></span> Active
       </>
     );
-  }
+  };
 
   const memberLogo = () => {
     return (
       <>
-        <span class="label label-rounded label-primary label-pill label-inline mr-2">member</span>
+        <span class="label label-rounded label-primary label-pill label-inline mr-2">
+          member
+        </span>
       </>
-    )
-  }
+    );
+  };
 
-  // คลิกแล้วไปไหน 
+  // คลิกแล้วไปไหน
   const rowEvents = {
     onClick: (e, column, columnIndex, row, rowIndex) => {
       // console.log(row.id)
       // window.location.href=`/testPage/${row.id}`
-      history.push(`/testPage/${row.id}`)
-    }
+      history.push(`/testPage/${row.id}`);
+    },
   };
 
-  const defaultSorted = [{
-    dataField: 'name',
-    order: 'ASC'
-  }];
+  const defaultSorted = [
+    {
+      dataField: "name",
+      order: "ASC",
+    },
+  ];
 
   const dataEmpty = () => {
     return (
       <>
-        <h3 className="text-center pt-5 mt-5">Table is Empty...</h3>
+        <button
+          type="button"
+          class="btn btn-outline-secondary spinner spinner-darker-dark spinner-right mt-5"
+        >
+          Loading ...
+        </button>
       </>
-    )
-  }
+    );
+  };
 
   const selectOptions = {
-    1: 'Member',
-    2: 'Artist',
+    1: "Member",
+    2: "Artist",
   };
 
   const width70PX = {
-     width: '70px'
-  }
-
+    width: "70px",
+  };
 
   const columns = [
     // { dataField: 'id', text: 'ID', classes: 'text-center align-middle', headerClasses: 'text-center' },
-    { dataField: 'name', text: 'Avatar', classes: '', headerClasses: '' , headerStyle: width70PX , formatter: Avater, },
-    { dataField: 'name', text: 'Name', classes: 'align-middle', sort: true },
-    { dataField: 'email', text: 'Email', classes: 'align-middle ', sort: true },
-    { dataField: 'phone', text: 'Phone', classes: 'align-middle ', sort: true },
-    { dataField: 'type', text: '', classes: 'align-middle', formatter: memberLogo, align: 'center', headerClasses: 'text-center', filter: selectFilter({options: selectOptions ,defaultValue: null}) },
-    { dataField: 'status', text: 'Status', formatter: iconOnline, classes: 'text-center align-middle', headerClasses: 'text-center', sort: true },
-    { dataField: 'action', text: 'Action', formatter: actionButton, events: rowEvents, classes: 'text-center align-middle', headerClasses: 'text-center' , headerStyle: width70PX },
-  ]
-
+    {
+      dataField: "name",
+      text: "Avatar",
+      classes: "",
+      headerClasses: "",
+      headerStyle: width70PX,
+      formatter: Avater,
+    },
+    { dataField: "name", text: "Name", classes: "align-middle", sort: true },
+    { dataField: "email", text: "Email", classes: "align-middle ", sort: true },
+    { dataField: "phone", text: "Phone", classes: "align-middle ", sort: true },
+    {
+      dataField: "type",
+      text: "",
+      classes: "align-middle",
+      formatter: memberLogo,
+      align: "center",
+      headerClasses: "text-center",
+      filter: selectFilter({ options: selectOptions, defaultValue: null }),
+    },
+    {
+      dataField: "status",
+      text: "Status",
+      formatter: iconOnline,
+      classes: "text-center align-middle",
+      headerClasses: "text-center",
+      sort: true,
+    },
+    {
+      dataField: "action",
+      text: "Action",
+      formatter: actionButton,
+      events: rowEvents,
+      classes: "text-center align-middle",
+      headerClasses: "text-center",
+      headerStyle: width70PX,
+    },
+  ];
 
   return (
     <div className="container">
-
-      <Card >
+      <Card>
         <Card.Body>
-          <ToolkitProvider
-            keyField="id"
-            data={member}
-            columns={columns}
-            search>
-            {props => (
+          <ToolkitProvider keyField="id" data={member} columns={columns} search>
+            {(props) => (
               <>
                 <h3>Member List</h3>
                 <hr />
-                 <SearchBar {...props.searchProps} />
-                 <br/>
-                 <small className="text-muted"> <b>Search</b> in all fields</small>
-                
+                <SearchBar {...props.searchProps} />
+                <br />
+                <small className="text-muted">
+                  {" "}
+                  <b>Search</b> in all fields
+                </small>
+
                 <BootstrapTable
                   {...props.baseProps}
                   wrapperClasses="table-responsive"
@@ -137,13 +166,12 @@ export default function TestPage() {
                   bootstrap4
                   // loading={true}
                   // overlay={ overlayFactory({ spinner: true, background: 'rgba(192,192,192,0.3)' }) }
-                  keyField='id'
+                  keyField="id"
                   // ข้อมูลในตาราง
                   data={member === null ? [] : member}
                   // รูปแบบตาราง
                   columns={columns}
                   bordered={false}
-
                   // ค่าเริ่มต้นให้ DESC หรือ ASC ตามที่กำหนด
                   // defaultSorted={defaultSorted}
 
@@ -152,13 +180,13 @@ export default function TestPage() {
                   // กรองข้อมูล
                   filter={filterFactory()}
                   // ตัวเลื่อนหน้า
-                  pagination={paginationFactory()} />
+                  pagination={paginationFactory()}
+                />
               </>
-            )
-            }
+            )}
           </ToolkitProvider>
         </Card.Body>
       </Card>
     </div>
-  )
+  );
 }
--- backoffice/src/components/pages/TestPage2.js
+++ backoffice/src/components/pages/TestPage2.js
@@ -1,17 +1,67 @@
-import React from 'react'
+import React from "react";
 export default function TestPage2(props) {
+  // ดึงค่าจาก /xxx?user:1234
+  // const search = new URLSearchParams(props.locations.search)
+  // console.log(props.match.params.id)
+  // console.log(search)
 
-    // ดึงค่าจาก /xxx?user:1234 
-    // const search = new URLSearchParams(props.locations.search)
-    // console.log(props.match.params.id)
-    // console.log(search)
+  // ดึงค่าไอดีจาก http /:id
+  const params = props.match.params.id;
+  return (
+    <div className="container">
+      {/* <h1>This id member : {params}</h1> */}
+      {/* <button
+        className="btn btn-sm font-weight-bold btn-primary"
+        onClick={() => {
+          props.history.goBack();
+        }}
+      >
+        Back Page
+      </button> */}
 
-    // ดึงค่าไอดีจาก http /:id 
-    const params = props.match.params.id
-    return (
-        <div className="container">
-            <h1>This id member : {params}</h1>
-            <button className="btn btn-sm font-weight-bold btn-primary" onClick={() => {props.history.goBack()}}>Back Page</button>
+      {/*begin::Content*/}
+      <div className="flex-row-fluid ml-lg-8">
+        {/*begin::Card*/}
+        <div className="card card-custom card-stretch">
+          {/*begin::Header*/}
+          <div className="card-header py-3">
+            <div className="card-title align-items-start flex-column">
+              <h3 className="card-label font-weight-bolder text-dark">
+                Member Information
+              </h3>
+              <span className="text-muted font-weight-bold font-size-sm mt-1">
+                Member personal informaiton
+              </span>
+            </div>
+            <div className="card-toolbar">
+              <button 
+               className="btn btn-sm font-weight-bold btn-primary"
+               onClick={() => {
+                 props.history.goBack();
+               }}
+              className="btn btn-secondary">
+                Back Page
+              </button>
+            </div>
+          </div>
+          {/*end::Header*/}
+          {/*begin::Form*/}
+          <form className="form">
+            {/*begin::Body*/}
+            <div className="card-body">
+              <div className="row">
+                <label className="col-xl-3" />
+                <div className="col-lg-9 col-xl-6">
+                  {/* <h5 className="font-weight-bold mb-6">Customer Info</h5> */}
+                </div>
+              </div>
+            </div>
+            {/*end::Body*/}
+          </form>
+          {/*end::Form*/}
         </div>
-    )
+      </div>
+      {/*end::Content*/}
+    </div>
+  );
 }
