92f1121a78316f5f61e3c560a3bd857348df5bd0
Arnon kijlerdphon <snappy.kop@gmail.com>
2020-06-05 13:18:31 +0700

    add firebase

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,8 @@
 // next.config.js
-const withImages = require('next-images')
+const withImages = require("next-images");
 module.exports = withImages({
+  distDir: "nextjs",
   webpack(config, options) {
-    return config
-  }
-})
+    return config;
+  },
+});
