bd84a41213fcae6c81117c7a48f1590234e088bf
HaritFluk <07ghostharitkumsan@gmail.com>
2020-06-09 16:51:30 +0700

    My Last.

--- web/component/socialLogin.js
+++ web/component/socialLogin.js
@@ -7,110 +7,68 @@ import Link from "next/link";
 import Firebase from "../api/functions/config/config";
 
 export default function SocialSignIn() {
+
+  let googleProvider = new Firebase.auth.GoogleAuthProvider();
+  let facebookProvider = new Firebase.auth.FacebookAuthProvider();
+  let twitterProvider = new Firebase.auth.TwitterAuthProvider();
+
+  const handleFacebook = () => {
+    Firebase.auth().signInWithPopup(facebookProvider).then(function(result) {
+      // Accounts successfully linked.
+      let credential = result.credential;
+      let user = result.user;
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      console.log(error)
+      // ...
+    });
+  }
+
+  const handleTwitter = () => {
+    Firebase.auth().signInWithPopup(twitterProvider).then(function(result) {
+      // Accounts successfully linked.
+      let credential = result.credential;
+      let user = result.user;
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      console.log(error)
+      // ...
+    });
+  }
+
+  const handleGoogle = () => {
+    Firebase.auth().signInWithPopup(googleProvider).then(function(result) {
+      // Accounts successfully linked.
+      let credential = result.credential;
+      let user = result.user;
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      console.log(error)
+      // ...
+    });
+  }
+
+
   return (
     <>
-      <Link href="/">
         <a
-          onClick={() => {
-            var provider = new Firebase.auth.FacebookAuthProvider();
-            provider.setCustomParameters({
-              display: "popup",
-            });
-            Firebase.auth()
-              .signInWithPopup(provider)
-              .then(function (result) {
-                // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-                var token = result.credential.accessToken;
-                // The signed-in user info.
-                var user = result.user;
-                // ...
-              })
-              .catch(function (error) {
-                // Handle Errors here.
-                var errorCode = error.code;
-                console.log(errorCode)
-                var errorMessage = error.message;
-                console.log(errorMessage)
-                // The email of the user's account used.
-                var email = error.email;
-                console.log(email)
-                // The firebase.auth.AuthCredential type that was used.
-                var credential = error.credential;
-                console.log(credential)
-                // ...
-              });
-          }}
+          onClick={handleFacebook}
         >
           <img src={Facebook} />
         </a>
-      </Link>
-      <Link href="/">
         <a
-          onClick={() => {
-            var provider = new Firebase.auth.TwitterAuthProvider();
-            Firebase.auth()
-              .signInWithPopup(provider)
-              .then(function (result) {
-                // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
-                // You can use these server side with your app's credentials to access the Twitter API.
-                var token = result.credential.accessToken;
-                var secret = result.credential.secret;
-                // The signed-in user info.
-                var user = result.user;
-                // ...
-              })
-              .catch(function (error) {
-                // Handle Errors here.
-                var errorCode = error.code;
-                console.log(errorCode)
-                var errorMessage = error.message;
-                console.log(errorMessage)
-                // The email of the user's account used.
-                var email = error.email;
-                console.log(email)
-                // The firebase.auth.AuthCredential type that was used.
-                var credential = error.credential;
-                console.log(credential)
-                // ...
-              });
-          }}
+          onClick={handleTwitter}
         >
           <img src={Twitter} />
         </a>
-      </Link>
-      <Link href="/">
         <a
-          onClick={() => {
-            var provider = new Firebase.auth.GoogleAuthProvider();
-            Firebase
-              .auth()
-              .signInWithPopup(provider)
-              .then(function (result) {
-                // This gives you a Google Access Token. You can use it to access the Google API.
-                var token = result.credential.accessToken;
-                // The signed-in user info.
-                var user = result.user;
-                // ...
-              })
-              .catch(function (error) {
-                // Handle Errors here.
-                var errorCode = error.code
-                console.log(errorCode)
-                var errorMessage = error.message
-                console.log(errorMessage)
-                // The email of the user's account used.
-                var email = error.email
-                console.log(email)
-                // The firebase.auth.AuthCredential type that was used.
-                var credential = error.credential
-                console.log(credential)
-                // ...
-              });
-          }}
+          onClick={handleGoogle}
         >
           <img src={Google} />
         </a>
-      </Link>
     </>
   );
 }
