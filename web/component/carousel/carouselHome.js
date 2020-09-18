4a6c8973b3065a3da4474777ae8da9cdebddaa32
Marutthep Rompho <teemmarutthep@gmail.com>
2020-09-18 16:38:47 +0700

    Recommeaded Music done

--- web/component/carousel/carouselHome.js
+++ web/component/carousel/carouselHome.js
@@ -11,22 +11,47 @@ const images = [
   "https://animatebkk-online.com/wp-content/uploads/2019/07/4988104115508-1.jpg",
   "https://cf.lnwfile.com/3cfb9j.jpg",
   "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSJIaFQicGfALu7nsov8BnW0uGwmREzgzgsAg&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSJIaFQicGfALu7nsov8BnW0uGwmREzgzgsAg&usqp=CAU",
   "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREZ2nF2ZsIW25Y1T7-PldKdEWb85X6o5ztyA&usqp=CAU",
   "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTybMm_awsAxS-JHGSj0EdlSg4rUSlhEXckiw&usqp=CAU",
   "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQf2__ZhxOcOVBSA1BMwNMI7o9qPFycwyTIVg&usqp=CAU",
+  "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/minimal-dark-album-cd-cover-template-design-d666beed411324dbdc8dc539de9cc27a_screen.jpg?ts=1579664445",
 ];
 
 const CarouselHomePage = ({ deviceType }) => {
-  const CustomButtonGroupAsArrows = ({ next, previous }) => {
+  const CustomButtonGroupAsArrows = ({ next, previous, ...rest }) => {
+    const {
+      carouselState: { currentSlide },
+    } = rest;
     return (
       <div
         style={{
-          textAlign: "center",
+          top: "-22rem",
         }}
+        className="row col-md-auto text-light "
       >
-        <button onClick={previous}>{"<"}</button>
-        <button onClick={next}>{">"}</button>
+        <h6 className="font-SemiBold h5 ">Recommended Music</h6>{" "}
+        <div className="row ml-auto">
+          <p
+            onClick={previous}
+            className={`ml-auto font-25 pointer mr-3`}
+          >
+            {" < "}
+          </p>
+          <p onClick={next} className="ml-auto font-25 pointer">
+            {"  > "}
+          </p>
+        </div>
       </div>
+
+      // <div
+      //   style={{
+      //     textAlign: "center",
+      //   }}
+      // >
+      //   <button onClick={previous}>{"<"}</button>
+      //   <button onClick={next}>{">"}</button>
+      // </div>
     );
   };
   const responsive = {
@@ -36,25 +61,25 @@ const CarouselHomePage = ({ deviceType }) => {
       paritialVisibilityGutter: 30,
     },
     subDesktop: {
-        breakpoint: { max: 1200, min: 1024 },
-        items: 4,
-      //   paritialVisibilityGutter: 50,
-      },
+      breakpoint: { max: 1200, min: 1024 },
+      items: 4,
+      paritialVisibilityGutter: 50,
+    },
     tablet: {
-      breakpoint: { max: 1024, min: 464 },
+      breakpoint: { max: 1024, min: 767 },
       items: 3,
-    //   paritialVisibilityGutter: 50,
+      paritialVisibilityGutter: 50,
     },
     mobile: {
-      breakpoint: { max: 464, min: 0 },
+      breakpoint: { max: 767, min: 0 },
       items: 2,
-    //   paritialVisibilityGutter: 30,
+      //   paritialVisibilityGutter: 30,
     },
   };
   return (
     <Carousel
       ssr
-    //   partialVisbile
+      // partialVisbile
       draggable
       swipeable
       centerMode={false}
@@ -65,22 +90,46 @@ const CarouselHomePage = ({ deviceType }) => {
       responsive={responsive}
       renderButtonGroupOutside={true}
       arrows={false}
+      autoPlay={false}
       customButtonGroup={<CustomButtonGroupAsArrows />}
     >
-      {images.map((image) => {
+      {images.map((image, index) => {
         return (
-            <>
-          <img
-            draggable={false}
-            style={{ width: "100%", height: "100%" }}
-            src={image}
-          />
-          <h1>dasdas</h1>
-          </>
+          <div key={index}>
+            <img
+              draggable={false}
+              style={{ width: "100%", height: "200px" }}
+              src={image}
+              className="mb-4"
+            />
+            <div className="text-light detail-recommended row ">
+              <div className="col-10 my-auto">
+                <h6>Good Bye</h6>
+              </div>
+              <div className="col-2 my-auto">
+                <span
+                  style={{
+                    fontSize: "35px",
+                  }}
+                  className="material-icons "
+                >
+                  play_circle_filled
+                </span>
+              </div>
+
+              <small className="small col-12">pop,rock</small>
+            </div>
+          </div>
         );
       })}
+      <style jsx>
+        {`
+          .detail-recommended {
+            max-width: 100%;
+          }
+        `}
+      </style>
     </Carousel>
-    
   );
 };
 
